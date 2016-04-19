bd1503bdcf62408462f969870cd80df66bd49cc4
Taylor Smith <taylorsmith4546@gmail.com>
2016-04-19 10:04:42 -0700

    initial commit

---
+++ Bubblesort/Bubblesort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubblesort
+{
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+            int[] arr = new int[6] {3, 5, 7, 6, 4, 8};
+
+            int temp = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("{0},", arr[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}   
+    
+    
+
